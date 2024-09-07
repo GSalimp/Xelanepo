dcf4406e2a48e3cbc6c46fa4214a97490c393b71
Guilherme Salim <guilhermesalim@192.168.5.129>
2024-09-07 18:24:05 -0300

    Home page

--- xelanepo/src/App.jsx
+++ xelanepo/src/App.jsx
@@ -12,7 +12,6 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <h1>Site</h1>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
--- xelanepo/src/components/Header.jsx
+++ xelanepo/src/components/Header.jsx
@@ -1,16 +1,17 @@
 // Header component
 import { Link } from "react-router-dom";
+import './styles/Header.css';
 
-function Header() {
-    return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/Search">Search</Link></li>
-                <li><Link to="/Profile/1">Profile</Link></li>
-            </ul>
-        </nav>
-    );
-}
+const Header = () => (
+    <header className="header">
+      <div className="logo">XELANEPO</div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/Search">App</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 
 export { Header };
