42a60f2fe8f13ff5433e141bdb0e7a00813182cb
Guilherme Salim <salim.guilherme2009@gmail.com>
2024-09-30 09:24:30 -0300

    paginação works

--- xelanepo/src/components/Profile/Works.jsx
+++ xelanepo/src/components/Profile/Works.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./../styles/ProfileItens/Works.css";
 
 function workCard(work, index) {
@@ -10,7 +10,7 @@ function workCard(work, index) {
         <div className="subtitle">
           {work.type} - {work.publication_year} - {work.open_access?.is_oa ? "Open Access" : "Closed Access"}
         </div>
-        <div className="citation">Cited by: {work.cited_by_count}</div> {/* Added citation count */}
+        <div className="citation">Cited by: {work.cited_by_count}</div>
       </div>
     </a>
   );
@@ -18,22 +18,37 @@ function workCard(work, index) {
 
 function Works({ worksApiUrl }) {
   const [works, setWorks] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchWorks() {
       try {
-        console.log("worksApiUrl", worksApiUrl)
-        const worksRequest = await fetch(worksApiUrl);
+        setLoading(true);
+        const worksRequest = await fetch(worksApiUrl + `&per_page=10&page=${page}`);
         const worksData = await worksRequest.json();
-        setWorks(worksData);
-        console.log("worksData", worksData)
-      } 
-      catch (error) {
+        setWorks((prevWorks) => [...prevWorks, ...worksData.results]);
+        setLoading(false);
+      } catch (error) {
         console.error(error);
+        setLoading(false);
       }
     }
     fetchWorks();
-  },[])
+  }, [page, worksApiUrl]);
+
+  useEffect(() => {
+    function handleScroll() {
+      const worksDiv = document.querySelector(".works");
+      const bottom = worksDiv && window.innerHeight + window.scrollY >= worksDiv.offsetTop + worksDiv.scrollHeight - 50;
+      if (bottom && !loading) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [loading]);
 
   if (works.length === 0 || worksApiUrl === "")
     return (
@@ -42,15 +57,16 @@ function Works({ worksApiUrl }) {
         <div className="loading"></div>
       </div>
     );
-  
-  works.results.sort((a, b) => b.cited_by_count - a.cited_by_count);
+
+  works.sort((a, b) => b.cited_by_count - a.cited_by_count);
 
   return (
     <div className="works profileItem">
       <span className="profileItemTitle">Works</span>
       <div className="work-itens">
-        {works.results.map((work, index) => workCard(work, index))}
+        {works.map((work, index) => workCard(work, index))}
       </div>
+      {loading && <div className="loading"></div>}
     </div>
   );
 }
